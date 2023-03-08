1db9dca91880c4a655ee1e31e0590b16c6e85f93
admin <moladeijarotimi@gmail.com>
2023-03-08 13:47:47 -0800

    project completed

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -33,6 +33,7 @@ const SearchBar = () => {
         name="search"
         value={contractAddress}
         onChange={handleSearch}
+        autocomplete="off"
         placeholder="Enter Contract Address"
         onKeyDown={handleKeyPress}
       />
